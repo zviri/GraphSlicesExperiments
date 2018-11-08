cf4bdc6c6e32e56b112e973ef1a7013244f063b6
Peter Zvirinsky <peter.zvirinsky@gmail.com>
2018-11-08 09:44:24 +0100

    Clean dataset step

--- holasek/clean_dataset.py
+++ holasek/clean_dataset.py
@@ -3,27 +3,31 @@ import dask.dataframe as dd
 import argparse
 import unicodedata
 import re
-from holasek.common import *
+from common import *
+import re
 
 if is_jupyter_env():
-    import sys; sys.argv=['', 'holasek/data/receivables/', 'data/receivables_cleaned/'];
+    import sys; sys.argv=['', '', ''];
 
 parser = argparse.ArgumentParser()
 parser.add_argument("receivables_path", help="Dataset path", type=str);
 parser.add_argument("output_path", help="Output path", type=str);
 args = parser.parse_args()
 
-logging.info("Loading receivables dataset...")
-
-receivables_dd = dd.read_csv(args.receivables_path + "*.csv", encoding="utf-8")
-
 def remove_accents(s):
     nkfd_form = unicodedata.normalize('NFKD', s)
     only_ascii = nkfd_form.encode('ASCII', 'ignore')
     return only_ascii
 
-logging.info("Removing accents...")
-receivables_df["content"] = receivables_df["content"].apply(lambda text: remove_accents(text).lower())
+def preprocess_df(df):
+    df["content"] = df["content"].fillna("")
+    df["content"] = df["content"].apply(
+        lambda content: remove_accents(re.sub(r"[©@]", "#", content)).lower().decode("utf-8") # need to special characters later
+    )
+    return df
 
-logging.info("Saving data to %s", args.output_path)
-receivables_df.to_csv(args.output_path, index=False, encoding="utf-8")
+logging.info("Removing accents...")
+receivables_dd = dd\
+.read_parquet(args.receivables_path + "*.parquet")\
+.map_partitions(preprocess_df)\
+.to_parquet(args.output_path, compression="gzip")
